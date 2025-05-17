fadb9b8b5aedfb5e9ac8b3c8fe7e8c0f0c6b4c86
pedrolopxs7 <pedro.gomes137@etec.sp.gov.br>
2025-05-16 21:55:50 -0300

    first commit

---
+++ app/(drawer)/home/_layout.tsx
@@ -0,0 +1,22 @@
+import Drawer from "expo-router/drawer";
+
+export default function DrawerLayout() {
+    return (
+        <Drawer>
+            <Drawer.Screen
+            name='home'
+            options={{
+                drawerLabel: 'Home',
+                title: 'Início'
+            }}
+            />
+            <Drawer.Screen
+            name='settings/index'
+            options={{
+                drawerLabel: 'Settings',
+                title: 'Configuração'
+            }}
+            />
+        </Drawer>
+    )
+}
