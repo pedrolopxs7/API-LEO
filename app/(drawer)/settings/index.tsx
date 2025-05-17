00d5971dd5a08aa62e5a2c4128ec7f956a4e6e8a
pedrolopxs7 <pedro.gomes137@etec.sp.gov.br>
2025-05-16 22:03:23 -0300

    segundo commit

---
+++ app/(drawer)/settings/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function SettingsScreen() {
+    return(
+        <View style={{
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center'
+        }}> 
+        <Text>Tela de configuração</Text>
+        </View>
+    )
+}
